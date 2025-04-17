cf4afb564288dc72d3b1f3b0499397a456e88700
tanupatil <tanupatil25022002@gmail.com>
2025-04-17 16:40:37 +0530

    Getting Started With StudentSelfReflection Backend Project.

---
+++ src/main/java/com/tanu/StudentSelfReflection/StudentSelfReflectionApplication.java
@@ -0,0 +1,13 @@
+package com.tanu.StudentSelfReflection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentSelfReflectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentSelfReflectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tanu/StudentSelfReflection/StudentSelfReflectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanu.StudentSelfReflection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentSelfReflectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
